19056445766cedb3be904c073eab94fe47a1f1dc
iannyip <ianyip@nullspace.com.sg>
2021-10-24 10:19:39 +0800

    setup db config

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: '<YOUR_USER_NAME>',
+    username: 'iannyip',
     password: null,
-    database: '<YOUR_DATABASE_NAME>',
+    database: 'homage_be',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
