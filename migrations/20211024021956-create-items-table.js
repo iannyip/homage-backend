7b107cdf0f213b4aab31c918a0507b05bc73c7c1
iannyip <ianyip@nullspace.com.sg>
2021-10-24 10:52:36 +0800

    added migration - 3 tables

---
+++ migrations/20211024021956-create-items-table.js
@@ -0,0 +1,102 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('centres', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      slot_capacity: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+      start_time: {
+        allowNull: false,
+        type: Sequelize.TIME,
+      },
+      end_time: {
+        allowNull: false,
+        type: Sequelize.TIME,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable('people', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      full_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      nric: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.createTable('bookings', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      person_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'people',
+          key: 'id',
+        },
+      },
+      centre_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'centres',
+          key: 'id',
+        },
+      },
+      time: {
+        allowNull: false,
+        type: Sequelize.TIME,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('bookings');
+    await queryInterface.dropTable('people');
+    await queryInterface.dropTable('centres');
+  },
+};
