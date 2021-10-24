fe0a254802c9c1f295c0219fada519e32335d320
iannyip <ianyip@nullspace.com.sg>
2021-10-24 12:35:52 +0800

    imported controllers and added planned routes to /routes.mjs

--- routes.mjs
+++ routes.mjs
@@ -1,11 +1,22 @@
 import db from './models/index.mjs';
 
-// import your controllers here
+// import the controller
+import initCentresController from './controllers/centres.mjs';
+import initPeopleController from './controllers/people.mjs';
+import initBookingsController from './controllers/bookings.mjs';
 
 export default function bindRoutes(app) {
+  // initialise controllers, pass in the db for all callbacks
+  const centresController = initCentresController(db);
+  const peopleController = initPeopleController(db);
+  const bookingsController = initBookingsController(db);
 
-  // initialize the controller functions here
-  // pass in the db for all callbacks
-
-  // define your route matchers here using app
+  // routes
+  app.get('/bookings', bookingsController.index);
+  app.get('/bookings/create', bookingsController.createForm);
+  app.post('/bookings/create', bookingsController.create);
+  app.get('/bookings/:id', bookingsController.show);
+  app.get('/bookings/:id/edit', bookingsController.editForm);
+  app.put('/bookings/:id/edit', bookingsController.update);
+  app.get('/bookings/:id/delete', bookingsController.destroy);
 }
