f2741bc3509c643bfd446864949b1622fae617f4
iannyip <ianyip@nullspace.com.sg>
2021-10-25 22:31:23 +0800

    modified showSlots action to also get the number of remaining slots for a day

--- controllers/centres.mjs
+++ controllers/centres.mjs
@@ -1,4 +1,7 @@
 import moment from 'moment';
+import pkg from 'sequelize';
+
+const { Op } = pkg;
 
 export default function initCentresController(db) {
   // For now, centres will have no controllers
@@ -26,26 +29,67 @@ export default function initCentresController(db) {
         response.send(204);
       }
 
+      const startDate = moment(date).startOf('day').toISOString();
+      const endDate = moment(date).endOf('day').toISOString();
+      console.log('==============');
+      console.log(`startDate: ${startDate}, endDate: ${endDate} `);
+
       // Get the particular centre's details
       const centreOperation = await db.Centre.findOne({
         where: { id },
         attributes: ['startTime', 'endTime', 'slotCapacity'],
       });
 
-      // Generate array of timeslots and capacity
-      const timeslots = [];
+      // Get all bookings for the given date
+      const bookingsOnDate = await db.Booking.findAll({
+        where: {
+          date: { [Op.between]: [startDate, endDate] },
+          centreId: id,
+        },
+        attributes: ['date', 'time'],
+      });
+      console.log('==============');
+      console.log(bookingsOnDate);
+
+      // Generate object of timeslots and capacity
+      // const timeslots = [];
+      const timeslotsObj = {};
       const startArr = centreOperation.startTime.split(':');
       const endArr = centreOperation.endTime.split(':');
       const slotCount = (endArr[0] - startArr[0]) * 4;
       for (let i = 0; i < slotCount; i += 1) {
-        timeslots.push({
-          time: moment().hours(startArr[0]).minutes(0).add(i * 15, 'm')
-            .format('HH:mm'),
-          capacity: centreOperation.slotCapacity,
+        timeslotsObj[moment().hours(startArr[0]).minutes(0).add(i * 15, 'm')
+          .format('HH:mm')] = centreOperation.slotCapacity;
+        // timeslots.push({
+        //   time: moment().hours(startArr[0]).minutes(0).add(i * 15, 'm')
+        //     .format('HH:mm'),
+        //   capacity: centreOperation.slotCapacity,
+        // });
+      }
+      console.log('~~~~~~~~~~1111~~~~~~~~~~');
+      console.log(timeslotsObj);
+      // Update capacity of timeslots object
+      for (let i = 0; i < bookingsOnDate.length; i += 1) {
+        console.log(`checking on ${bookingsOnDate[i].time.substr(0, 5)}`);
+        timeslotsObj[bookingsOnDate[i].time.substr(0, 5)] -= 1;
+      }
+      console.log('~~~~~~~~~~2222~~~~~~~~~~');
+      console.log(timeslotsObj);
+
+      // convert timeslots object to an array
+      const timeslotsArr = [];
+      const timingsArr = Object.keys(timeslotsObj);
+      const capacityArr = Object.values(timeslotsObj);
+      for (let i = 0; i < timingsArr.length; i += 1) {
+        timeslotsArr.push({
+          time: timingsArr[i],
+          capacity: capacityArr[i],
         });
       }
 
-      response.send(timeslots);
+      console.log(timeslotsArr);
+
+      response.send(timeslotsArr);
     } catch (error) {
       console.log(error);
     }
