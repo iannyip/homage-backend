9adffa80c91ca4cfc0784cac9491ae5dd3095fd7
iannyip <ianyip@nullspace.com.sg>
2021-10-25 13:56:31 +0800

    updated centres controller index action to add 0 index item to array

--- controllers/centres.mjs
+++ controllers/centres.mjs
@@ -2,7 +2,10 @@ export default function initCentresController(db) {
   // For now, centres will have no controllers
   const index = async (request, response) => {
     try {
-      const allCentres = await db.Centre.findAll();
+      const allCentres = await db.Centre.findAll({
+        attributes: ['id', 'name'],
+      });
+      allCentres.push({ name: 'None', id: 0 });
       response.send(allCentres);
     } catch (error) {
       console.log(error);
